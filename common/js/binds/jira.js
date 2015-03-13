33e52b86358c67e25a2dcb20314909ed745f7aa5
Ian wallis <ian.wallis@ink-global.com>
2015-03-13 17:44:55 +0000

    Add boilerplate bind file for Jira

---
+++ common/js/binds/jira.js
@@ -0,0 +1,22 @@
+
+
+jQuery('document').ready(function(){
+
+    var App = {
+
+        init: function(){
+            this.injectAddTaskButton();
+        },
+
+        injectAddTaskButton: function() {
+
+        },
+
+        onAddTask: function(event){
+
+        }
+    }
+
+
+
+});
